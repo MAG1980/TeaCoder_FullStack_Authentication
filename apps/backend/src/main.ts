71b9e6f19b764b8746d3081baf64c9150773fda6
MAG1980 <al-1980@inbox.ru>
2025-04-30 02:56:23 +0300

    Выполнена настройка Redis. Добавлены утилиты для парсинга количества милисекунд и логических значений из строк.

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -8,16 +8,44 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app/app.module'
 import cookieParser from 'cookie-parser'
 import { ConfigService } from '@nestjs/config'
+import IORedis from 'ioredis'
+import session from 'express-session'
+import { ms, StringValue } from './lib/common/utils/ms.util'
+import { parseBoolean } from './lib/common/utils/parse-boolean.util'
+import { RedisStore } from 'connect-redis'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
   const config = app.get(ConfigService)
+  const redisUri = config.getOrThrow<string>('REDIS_URI')
+  console.log({ redisUri })
+  const redis = new IORedis(config.getOrThrow<string>('REDIS_URI'))
 
   app.use(cookieParser(config.getOrThrow<string | string[]>('COOKIES_SECRET')))
 
   app.useGlobalPipes(new ValidationPipe({
     transform: true,
+  }))
+
+  app.use(session({
+    secret: config.getOrThrow<string>('SESSION_SECRET'),
+    name: config.getOrThrow<string>('SESSION_NAME'),
+    resave: true,
+    // Не сохранять неинициализированные сессии (не содержащие userId).
+    saveUninitialized: false,
+    cookie: {
+      domain: config.getOrThrow<string>('SESSION_DOMAIN'),
+      maxAge: ms(config.getOrThrow<StringValue>('SESSION_MAX_AGE')),
+      httpOnly: parseBoolean(config.getOrThrow<string>('SESSION_HTTP_ONLY')),
+      secure: parseBoolean(config.getOrThrow<string>('SESSION_SECURE')),
+      sameSite: 'lax',
+    },
+    store: new RedisStore({
+      client: redis,
+      //Путь (префикс) к ключам сессий в Redis.
+      prefix: config.getOrThrow<string>('SESSION_FOLDER'),
+    }),
   }))
 
   app.enableCors({
@@ -32,7 +60,7 @@ async function bootstrap() {
   await app.listen(port)
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
-    `POSTGRES_URI= ${config.getOrThrow<string>("POSTGRES_URI")}`
+    `POSTGRES_URI= ${config.getOrThrow<string>('POSTGRES_URI')}`,
   )
 }
 
