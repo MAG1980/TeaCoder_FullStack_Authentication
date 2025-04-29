41c18ef4dfe9f80e9b3a9181066a1c93536ff36c
MAG1980 <al-1980@inbox.ru>
2025-04-30 00:07:54 +0300

    Настроен main.ts. В приложение глобально подключен ConfigModule.

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -1,6 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { IS_DEV_ENV } from '../lib/common/utils/is-dev.util'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      //Игнорировать .env в Production.
+      ignoreEnvFile: !IS_DEV_ENV,
+      isGlobal: true,
+    }),
+  ],
 })
 export class AppModule {}
--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,19 +3,36 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app/app.module';
+import { Logger, ValidationPipe } from '@nestjs/common'
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app/app.module'
+import cookieParser from 'cookie-parser'
+import { ConfigService } from '@nestjs/config'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 5000;
-  await app.listen(port);
+  const app = await NestFactory.create(AppModule)
+
+  const config = app.get(ConfigService)
+
+  app.use(cookieParser(config.getOrThrow<string | string[]>('COOKIES_SECRET')))
+
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true,
+  }))
+
+  app.enableCors({
+    origin: config.getOrThrow<string>('ALLOWED_ORIGIN'),
+    credentials: true,
+    exposedHeaders: ['set-cookie'],
+  })
+
+  const globalPrefix = 'api'
+  app.setGlobalPrefix(globalPrefix)
+  const port = config.getOrThrow<number>('APPLICATION_PORT') || 5000
+  await app.listen(port)
   Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
+  )
 }
 
-bootstrap();
+bootstrap()
