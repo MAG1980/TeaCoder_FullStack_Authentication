0ea89de81b63e6ec240139414c23801698a8bc08
MAG1980 <al-1980@inbox.ru>
2025-05-10 03:43:47 +0300

    В UserController создан пример использования декоратора Authorization().

--- apps/backend/src/user/user.controller.ts
+++ apps/backend/src/user/user.controller.ts
@@ -1,7 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus, Param } from '@nestjs/common'
 import { UserService } from './user.service';
+import { AuthorizedUser } from '../auth/decorators/authorized-user.decorator'
+import { Authorization } from '../auth/decorators/authorization.decorator'
+import { UserRole } from '../../prisma/__generated__'
 
-@Controller('user')
+@Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Authorization()
+  @HttpCode(HttpStatus.OK)
+  @Get('profile')
+  findProfile(@AuthorizedUser('id')userId: string){
+    return this.userService.findById(userId)
+  }
+
+  @Authorization(UserRole.ADMIN)
+  @HttpCode(HttpStatus.OK)
+  @Get(':id')
+  findById(@Param('id') id: string){
+    return this.userService.findById(id)
+  }
 }
