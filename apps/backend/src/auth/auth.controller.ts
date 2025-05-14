9b2820d377e48faf0e36c7a060ce93775605797a
MAG1980 <al-1980@inbox.ru>
2025-05-14 23:49:13 +0300

    Добавлена Google Recaptcha.

--- apps/backend/src/auth/auth.controller.ts
+++ apps/backend/src/auth/auth.controller.ts
@@ -4,17 +4,20 @@ import { HttpStatusCode } from 'axios'
 import { RegisterDto } from './dto/register.dto'
 import type { Request, Response } from 'express'
 import { LoginDto } from './dto/login.dto'
+import { Recaptcha } from '@nestlab/google-recaptcha'
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Recaptcha()
   @Post('register')
   @HttpCode(HttpStatusCode.Ok)
   async register(@Req() req: Request, @Body() registerDto: RegisterDto) {
     return await this.authService.register(req, registerDto)
   }
 
+  @Recaptcha()
   @Post('login')
   @HttpCode(HttpStatusCode.Ok)
   async login(@Req() req: Request, @Body() loginDto: LoginDto) {
--- apps/backend/src/auth/auth.module.ts
+++ apps/backend/src/auth/auth.module.ts
@@ -1,10 +1,20 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
+import { Module } from '@nestjs/common'
+import { AuthService } from './auth.service'
+import { AuthController } from './auth.controller'
 import { UserModule } from '../user/user.module'
+import { GoogleRecaptchaModule } from '@nestlab/google-recaptcha'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { getRecaptchaConfig } from '../configs/recapthcha.config'
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    GoogleRecaptchaModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: getRecaptchaConfig,
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
